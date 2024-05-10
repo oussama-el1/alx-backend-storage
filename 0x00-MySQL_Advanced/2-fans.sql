2566ac57642e8e29f9e65efb58fcf9149e22007a
oussama-el1 <elhadraoui284@gmail.com>
2024-05-10 03:59:35 +0000

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,4 @@
 -- Write a SQL script that ranks country origins of bands
-CREATE INDEX origin_fans_idx ON metal_bands (origin, fans);
-
-
 SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
