09fdfddd0aae92155dc701de58af8734e63f0115
oussama-el1 <elhadraoui284@gmail.com>
2024-05-11 06:23:07 +0000

    101 task

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,14 +1,14 @@
 -- update all students
 
 DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	UPDATE users
-    set average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
-    						INTO avg_weighted_score
-    						FROM corrections
-    						INNER JOIN projects ON corrections.project_id = projects.id
-    						WHERE corrections.user_id = user_idi;
-			)
+    UPDATE users set average_score = (SELECT
+    SUM(corrections.score * projects.weight) / SUM(projects.weight)
+    FROM corrections
+    INNER JOIN projects
+    ON projects.id = corrections.project_id
+    where corrections.user_id = users.id);
 END $$
 DELIMITER ;
-
