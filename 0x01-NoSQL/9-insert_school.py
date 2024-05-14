8115f52597b20d78f215e42ed3956489b0b9004f
oussama-el1 <elhadraoui284@gmail.com>
2024-05-15 00:14:13 +0100

    .\9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,5 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
   """ insert a new document in a collection """
-  return mongo_collection.insert_one(kwargs)
+  new_doc = mongo_collection.insert_one(kwargs)
+  return new_doc.inserted_id
