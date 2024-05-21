52df6a24065845408710565fbbd6d433d6d35c0f
oussama-el1 <elhadraoui284@gmail.com>
2024-05-21 19:58:18 +0100

    .\exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ method to store a data in a uuid4 key """
         key = str(uuid4())
         self._redis.set(key, data)
