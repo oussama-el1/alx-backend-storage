182fd0c8afdb5b873872c4ff2ea2d168750eeaca
oussama-el1 <elhadraoui284@gmail.com>
2024-05-21 22:57:44 +0100

    replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,8 +36,30 @@ def call_history(method: Callable) -> Callable:
             self._redis.rpush(keyout, out)
 
         return out
-
     return wrapper
+
+
+def replay(method: Callable) -> None:
+    """ replay the history """
+    redis_client = redis.Redis()
+    methodname = method.__qualname__
+    listin = redis_client.lrange(f"{methodname}:inputs", 0, -1)  # nopep8
+    listout = redis_client.lrange(f"{methodname}:outputs", 0, -1)  # nopep8
+
+    in_and_out = zip(listin, listout)
+    in_and_out_list = list(in_and_out)  # Store the result of zip in a list
+    print(f"Cache.store was called {len(in_and_out_list)} times:")
+    for invalue, outvalue in in_and_out_list:
+        # Convert binary strings to regular strings
+        if isinstance(invalue, bytes):
+            invalue_str = f"{invalue.decode('utf-8')}"
+        elif isinstance(invalue, int):
+            invalue_str = f"{invalue}"
+        else:
+            invalue_str = f"{invalue}"
+
+        outvalue_str = outvalue.decode('utf-8')
+        print(f"{methodname}(*{invalue_str}) -> {outvalue_str}")
 
 
 class Cache:
